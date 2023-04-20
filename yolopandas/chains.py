c70e47e5b2fd8c832e547f1f4aa3e46a618f3a83
DanteNoguez <dantenoguez@gmail.com>
2023-04-19 20:35:08 -0600

    w

--- yolopandas/chains.py
+++ yolopandas/chains.py
@@ -47,7 +47,8 @@ This is the history of your interaction so far:
 {chat_history}
 Human: {query}
 
-If several results match the same criteria from the query, return only the top 10 by using .head(10).
+If several results match the same criteria from the query, return all of them in a DataFrame.
+If the result is an individual number, return it as a float64.
 
 Go!
 
